4e5ae80038195545c7f79348fd4bb2997f3f095a
lesal1831648 <lesal1831648@etu.cegepjonquiere.ca>
2018-10-26 14:53:02 -0400

    Exercices Ciné Fini

--- Exercices JS/CinéClub - V2/JS/traitement.js
+++ Exercices JS/CinéClub - V2/JS/traitement.js
@@ -3,11 +3,13 @@ function btnCalculer_onclick() {
     var Age;
     var Rabais;
     var Total;
+    var Heure;
     var MiSession;
     var Film;
 
     PrixBase = parseFloat(document.getElementById("txtBasePrix").value);
     Age = parseFloat(document.getElementById("txtAge").value);
+    Film = document.getElementById("lstFilm").value;
 
     if (Age <= 16)
     {
@@ -15,11 +17,24 @@ function btnCalculer_onclick() {
     if (document.getElementById("radMidi").checked == true)
         {
         Rabais = 1.60;
+        Heure = "Midi"
         }
 
     else
         {
         Rabais = 1.40;
+        Heure = "Soir"
+        }
+
+        if (document.getElementById("MiSession").checked == true)
+        {
+            MiSession = 1.20;
+
+        }
+
+        else
+        {
+            MiSession = 1;
         }
 
     }
@@ -30,19 +45,31 @@ function btnCalculer_onclick() {
         if (document.getElementById("radMidi").checked == true)
         {
             Rabais = 1.30;
+            Heure = "Midi"
         }
 
         else
         {
-            Rabais = 0;
+            Rabais = 1;
+            Heure = "Soir"
         }
 
+        if (document.getElementById("MiSession").checked == true)
+        {
+            MiSession = 1.20;
+        }
+
+        else
+        {
+            MiSession = 1;
+        }
     }
 
     Total = PrixBase / Rabais;
+    Total = Total / MiSession;
 
     console.log(Total.toFixed(2));
-    console.log("Le total est de: " +Total);
+    console.log("Un billet du " +Heure+ " pour un spectateur de " +Age+ " coûte " +Total+ " $ pour le film " +Film+ ".");
 
 
 
