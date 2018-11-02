654b5e6cc526ba687b588f25ae6a42b5cc4b375f
lesal1831648 <lesal1831648@etu.cegepjonquiere.ca>
2018-11-02 14:33:19 -0400

    Garderie chien

--- Exercices JS/GarderieAnimaux/JS/traitement.js
+++ Exercices JS/GarderieAnimaux/JS/traitement.js
@@ -6,10 +6,22 @@ function btnCalculer_onclick()
  	var Toilettage;
  	var Animal;
  	var Total;
+ 	var RabaisJ;
+ 	var NbreHeure;
+ 	var Salaire;
 
     Veterinaire = document.getElementById("lstVeterinaire").value;
     NbJour = parseFloat(document.getElementById("txtNbreJours").value);
+    NbreHeure = parseFloat(document.getElementById("txtNbreHeure").value);
 
+
+    switch (Veterinaire)
+    {
+        case "Audrey Bouchard": Salaire = 25; break;
+        case "Stéphane Tremblay": Salaire = 35; break;
+        case "Maxime Simard": Salaire = 40; break;
+        case "Mélissa Caron": Salaire = 45; break;
+    }
 
     if (document.getElementById("chkServ").checked == true)
     {
@@ -36,9 +48,40 @@ function btnCalculer_onclick()
 
     }
 
-    Total = (Total * NbJour * 1.14975) + Service ;
+    if (NbJour < 5)
+    {
+        RabaisJ = 1;
+    }
+
+    else if (NbJour >= 5 && NbJour < 10)
+    {
+        RabaisJ = 1.05;
+    }
+
+    else if (NbJour >= 10 && NbJour < 30)
+    {
+        RabaisJ = 1.1;
+    }
+
+    else if (NbJour > 30)
+    {
+        RabaisJ = 1.15;
+    }
+
+    Total = (Total * 1.14975 * Salaire * NbreHeure) + Service;
+    Total = Total / RabaisJ;
 
 
 
-    console.log("Vétérinaire responsable est " +Veterinaire+ ". Le montant pour la garde de votre " +Animal+ " est de " +Total.toFixed(2)+ " pour " +NbJour+ ", " +Toilettage+ " service de toilettage.");
+    console.log("Vétérinaire responsable est " +Veterinaire+ ". Le montant pour la garde de votre " +Animal+ " est de " +Total.toFixed(2)+ " pour " +NbJour+ " jours, " +Toilettage+ " service de toilettage.");
+}
+
+function btnAfficher_onclick()
+{
+    document.getElementById("img1").src = "img/boxer-mix-mongrel-from-cdc6-diaporama.jpg";
+}
+
+function btnAfficher2_onclick()
+{
+    document.getElementById("img2").src = "img/chat-double-face-chimère-700x460.jpg";
 }
