31bbc9b3121aecde8de4e3fc70f8989a1216d90d
lesal1831648 <lesal1831648@etu.cegepjonquiere.ca>
2018-11-09 15:36:16 -0500

    in progress

--- Exercices JS/Exercice _ Joueurs/JS/traitement.js
+++ Exercices JS/Exercice _ Joueurs/JS/traitement.js
@@ -1,68 +1,107 @@
-var Nom, NbrNom, Points, Moyenne, meilleurPointage, i;
-var tabPoint = new Array(NbrNom);
-var tabJoueur = new Array(NbrNom);
-
-function btnRechercher_onclick()
-{
-calculerJoueurs();
+var CptJoueur = 0;
+var tabPoint = new Array(4);
+var tabJoueur = new Array(4);
 
 
-}
 
 function btnAjouter_onclick()
-{ 
-
-
-}
-
-function btnTrouverPire_onclick()
 {
 
-
+    var Nom =document.getElementById("btnRechercher".value);
+    var Points = parseFloat(document.getElementById("txtPoints").value);
+    tabJoueur[CptJoueur] = Nom;
+    tabPoint[CptJoueur] = Points;
+    CptJoueur ++;
+    ActiverOption();
+    document.getElementById("lblNbreJoueur").innerHTML = "Numéro du joueur "+(CptJoueur + 1);
 }
 
-function btnTrouverMeilleur_onclick()
+function ActiverOption()
 {
-
+    if (CptJoueur > 3)
+    {
+        document.getElementById("btnTrouverMoy").disabled = false;
+        document.getElementById("btnAjouter").disabled = true;
+        document.getElementById("btnTrouverMeilleur").disabled = false;
+        document.getElementById("btnTrouverPire").disabled = false;
+        document.getElementById("btnRechercher").disabled = false;
+    }
 
 }
 
 function btnTrouverMoy_onclick()
 {
-
-
-}
-
-function saisirInfo()
-{
-    Nom =document.getElementById("btnRechercher".value);
-    Points = parseInt(document.getElementById("txtPoints").value);
-}
-
-function calculerJoueurs()
-{
-    NbrNom += 1;
-    document.getElementById("lblJoueur").innerHTML = "Le nom du joueur est: " + NbrNom;
-}
-
-function saisirMoy()
-{
-    for (i = 0; i < tabPoint.length; i++)
+    var i, Moyenne = 0;
+    for (i=0;i<tabJoueur.length; i++)
     {
-        Moyenne+= Points;
+        Moyenne+= tabPoint[i];
     }
-    Moyenne = Moyenne / tabPoint.length;
-    document.getElementById("lblReponse").innerHTML = "La moyenne est de:  "+Moyenne;
+    Moyenne = Moyenne/tabJoueur.length;
+    console.log("La moyenne est de " +Moyenne);
+    document.getElementById("lblReponse").innerHTML = "La moyenne est de " +Moyenne;
 }
 
-function saisirMeilleurPointage()
+function btnTrouverMeilleur_onclick()
 {
-    for (i = 0; i < tabPoint.length; i++)
+    var i, PtsMeilleur = 0, NomMeilleur, Meilleur;
+    for (i = 0; i < tabJoueur.length; i++)
     {
-        if (i <= tabPoint[i])
+        if (Meilleur <= tabPoint[i])
         {
-            meilleurPointage = i;
+            Meilleur = tabPoint[i];
+            PtsMeilleur = tabPoint[i];
+            NomMeilleur = tabJoueur[i];
         }
     }
-    document.getElementById("lblReponse").innerHTML = "Le meilleur pointage est:  "+meilleurPointage+" réalisé par "+NbrNom;
+    console.log("Le meilleur pointage est de " +PtsMeilleur);
+    document.getElementById("lblReponse").innerHTML = "Le meilleur joueur est " +NomMeilleur+ " qui a obtenu la note de " +PtsMeilleur;
 }
+
+function btnTrouverPire_onclick()
+{
+        var i, PtsPire = 0, NomPire, Pire;
+        for (i = 0; i < tabJoueur.length; i++)
+        {
+            if (Pire >= tabPoint[i])
+            {
+                Pire = tabPoint[i];
+                PtsPire = tabPoint[i];
+                NomPire = tabJoueur[i];
+            }
+        }
+        console.log("Le pire pointage est de " +PtsPire);
+        document.getElementById("lblReponse").innerHTML = "Le pire joueur est " + NomPire+" qui a obtenu la note de " +PtsPire;
+}
+
+    function btnRechercher_onclick()
+    {
+        var NomRechercher = document.getElementById("txtNom").value;
+        var TrouverNom = false;
+
+        while (TrouverNom == false)
+        {
+            var i=0;
+            if (NomRechercher== tabJoueur[i] && tabJoueur[i] <4)
+            {
+                document.getElementById("lblReponse").innerHTML = " Le score du joueur " +NomRechercher+ " est de " +tabPoint[i];
+                TrouverNom = true;
+            }
+
+            else
+            {
+                i++;
+            }
+
+        }
+
+        if (TrouverNom == true)
+        {
+            document.getElementById("lblReponse").innerHTML = "Le nombre de point de" +tabJoueur[i]+ " est de " +tabPoint[i];
+        }
+
+        else
+            {
+            document.getElementById("lblReponse").innerHTML = "Le nom que vous chercher est introuvable."
+            }
+    }
+
